d9b7d38547d911a8547986113475b47ce6c639da
Cristi846 <toadercristian2003@gmail.com>
2024-08-03 14:46:21 +0300

    H2 Console

--- spring-6-webapp/src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +13,12 @@ import org.springframework.stereotype.Component;
 public class BootstrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -31,25 +35,41 @@ public class BootstrapData implements CommandLineRunner {
         Book dddSaved = bookRepository.save(ddd);
 
         Author david = new Author();
-        eric.setFirstName("David");
-        eric.setLastName("Dip");
+        david.setFirstName("David");
+        david.setLastName("Dip");
 
         Book acR = new Book();
-        ddd.setTitle("Acient Rome");
-        ddd.setIsbn("654");
+        acR.setTitle("Acient Rome");
+        acR.setIsbn("654");
 
         Author davidSaved = authorRepository.save(david);
         Book acRSaved = bookRepository.save(acR);
 
         ericSaved.getBooks().add(dddSaved);
         davidSaved.getBooks().add(acRSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        acRSaved.getAuthors().add(davidSaved);
+
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("My Publisher");
+        publisher.setAddress("123 Main");
+        Publisher savedPublisher = publisherRepository.save(publisher);
+
+        dddSaved.setPublisher(savedPublisher);
+        acRSaved.setPublisher(savedPublisher);
 
         authorRepository.save(ericSaved);
         authorRepository.save(davidSaved);
+        bookRepository.save(dddSaved);
+        bookRepository.save(acRSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+
+
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
 
